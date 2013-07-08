be340fc19653e3802e3bc15e7c16733d29f1f4de
Leung4080 <Leung4080@gmail.com>
2013-07-08 15:46:56 +0800

    create

---
+++ NetSpeed.sh
@@ -0,0 +1,52 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: NetSpeed.sh
+# 
+#         USAGE: ./NetSpeed.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: YOUR NAME (), 
+#  ORGANIZATION: 
+#       CREATED: 2013/7/8 15:46:28 中国标准时间
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+
+while [ "1" ]
+  do
+    eth=$1
+    RXpre=$(cat /proc/net/dev | grep $eth | tr : " " | awk '{print $2}')
+    TXpre=$(cat /proc/net/dev | grep $eth | tr : " " | awk '{print $10}')
+    sleep 1
+    RXnext=$(cat /proc/net/dev | grep $eth | tr : " " | awk '{print $2}')
+    TXnext=$(cat /proc/net/dev | grep $eth | tr : " " | awk '{print $10}')
+    clear
+    echo  -e  "\t RX `date +%k:%M:%S` TX"
+    RX=$((${RXnext}-${RXpre}))
+    TX=$((${TXnext}-${TXpre}))
+
+    if [[ $RX -lt 1024 ]];then
+      RX="${RX}B/s"
+    elif [[ $RX -gt 1048576 ]];then
+      RX=$(echo $RX | awk '{print $1/1048576 "MB/s"}')
+    else
+      RX=$(echo $RX | awk '{print $1/1024 "KB/s"}')
+    fi
+
+    if [[ $TX -lt 1024 ]];then
+      TX="${TX}B/s"
+    elif [[ $TX -gt 1048576 ]];then
+      TX=$(echo $TX | awk '{print $1/1048576 "MB/s"}')
+    else
+      TX=$(echo $TX | awk '{print $1/1024 "KB/s"}')
+    fi
+
+    echo -e "$eth \t $RX   $TX "
+done
